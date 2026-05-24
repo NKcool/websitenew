3f976e4f5b072388acc00b74c66f1fd932f4c3b5
Naveen <nkcool3956@gmail.com>
2026-05-24 12:12:04 +0530

    local changes

--- src/data.ts
+++ src/data.ts
@@ -3,7 +3,7 @@ export const PROJECTS = [
     id: 'bindu-ai',
     title: 'Bindu AI - Personal Voice Assistant',
     description: 'Built Bindu AI, a personal AI assistant, using Python scripting and integration with the OpenAI API that uses 175B parameters. Bindu AI operates with voice commands and performs tasks according to user instructions including 15+ custom commands. It offers a range of functionalities, including answering questions, providing weather updates, and is available 24/7 for your service.',
-    image: '/bindu-ai.jpg',
+    image: '/bindu-ai.png',
     link: '#',
     tags: ['Python', 'OpenAI API', 'Voice Automation']
   },
@@ -19,7 +19,7 @@ export const PROJECTS = [
     id: 'lang-learning',
     title: 'Language Learning Website',
     description: 'Created a feature-rich language learning platform, including user-friendly quizzes, secure account authentication, and an engaging leaderboard to foster healthy competition. Resulted in a 20% increase in retention and a 25% boost in mobile engagement.',
-    image: '/lang-learning.jpg',
+    image: '/lang-learning.png',
     link: 'https://languagelearning.onrender.com/',
     tags: ['Web Development', 'Auth', 'Gamification']
   },
@@ -27,7 +27,7 @@ export const PROJECTS = [
     id: 'image-gallery',
     title: 'Image Gallery',
     description: 'Explore a world of stunning visuals with our Image Gallery app, using the Unsplash API. Discover high-quality, royalty-free images effortlessly. Your gateway to endless inspiration and beautiful wallpapers.',
-    image: '/image-gallery.jpg',
+    image: '/image-gallery.png',
     link: 'https://nkcool.github.io/ImageGallery/',
     tags: ['API Integration', 'UI/UX']
   },
@@ -35,7 +35,7 @@ export const PROJECTS = [
     id: 'farm-rent',
     title: 'Farm Rent (Group Project)',
     description: 'Access essential farm equipment effortlessly with our app. Browse, compare, and rent tractors and more, fostering efficiency in agriculture. Say goodbye to ownership hassles and embrace a flexible, cost-effective approach to farming.',
-    image: '/farm-rent.jpg',
+    image: '/farm-rent.png',
     link: 'https://devendradhote.github.io/Hackathon_codebite/',
     tags: ['Web App', 'Team Project', 'Marketplace']
   }
